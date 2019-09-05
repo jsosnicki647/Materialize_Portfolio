53f1c070672d2020c4f589d94ef99c0a958a067c
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-09-05 17:03:27 -0400

    initialized portfolio page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 $(document).ready(() =>{
 
     $(".contact-page-link").on("click", () =>{
-        $("#contact-form").show()
+        $("#portfolio-text").hide()
         $("#main-text").hide()
+        $("#contact-form").show()
+        console.log("click")
     })
 
     $(".about-page-link").on("click", () =>{
         $("#contact-form").hide()
+        $("#portfolio-text").hide()
         $("#main-text").show()
     })
+
+    $(".portfolio-page-link").on("click", () =>{
+        $("#contact-form").hide()
+        $("#main-text").hide()
+        $("#portfolio-text").show()
+    })
+
+
 })

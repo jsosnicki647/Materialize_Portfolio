cb3bbbc2d4deaa196bfa9b4b30e28e1879d198fc
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-09-10 08:27:42 -0400

    added nodemailer

--- js/script.js
+++ js/script.js
@@ -1,23 +1,48 @@
-$(document).ready(() =>{
+var nodemailer = require('nodemailer')
 
-    $(".contact-page-link").on("click", () =>{
+$(document).ready(() => {
+
+    $(".contact-page-link").on("click", () => {
         $("#portfolio-text").hide()
         $("#main-text").hide()
         $("#contact-form").show()
         console.log("click")
     })
 
-    $(".about-page-link").on("click", () =>{
+    $(".about-page-link").on("click", () => {
         $("#contact-form").hide()
         $("#portfolio-text").hide()
         $("#main-text").show()
     })
 
-    $(".portfolio-page-link").on("click", () =>{
+    $(".portfolio-page-link").on("click", () => {
         $("#contact-form").hide()
         $("#main-text").hide()
         $("#portfolio-text").show()
     })
 
+    $("#btn-email-submit").on("click", () => {
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'jesse.sosnicki@gmail.com',
+                pass: 'Romaglia89'
+            }
+        })
 
+        var mailOptions = {
+            from: $("#input-email").val(),
+            to: 'jesse.sosnicki@gmail.com',
+            subject: $("#input-subject").val(),
+            text: $("#text-area").val()
+        }
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        })
+    })
 })
